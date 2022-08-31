cd72fc2103b8136b9203be33e217bcd153fabd1e
Dani <dani@example.com>
2022-08-31 10:48:13 -0500

    MI TAREA

--- app/app.c
+++ app/app.c
@@ -1,4 +1,18 @@
+#include "pointers.h"
 int main(void)
 {
-  return 0;
+    int count = 8;
+    int arr[] = { 40, 30, 10, 70, 50, 20, 60, 10 };
+    int arr2[] = { 40, 30, 10, 70, 50, 20, 60, 10 };
+    int arr3[] = { 40, 30, 10, 70, 50, 20, 60, 10 };
+    int arr4[] = { 40, 30, 10, 70, 50, 20, 60, 10 };
+
+     bubble_sort(arr, count);
+     insertion_sort(arr2, count);
+     reverse(arr3, count);
+     int x = 0;
+     calulate_frequency(arr4, count, x);
+
+    return 0;
+
 }
--- lib/pointers.c
+++ lib/pointers.c
@@ -5,23 +5,107 @@
 
 #include "pointers.h"
 
-int bubble_sort(int *target, int count)
+int bubble_sort(int *collection, int count)
 {
-  // aqui su implementación.
-  return 0;
+    printf("BUBBLE\n");
+    int a, b, c;
+    int contador = 0;
+
+    for (a = 0; a < count; a++) {
+        for (b = a + 1; b < count; b++) {
+            if (*(collection + b) < *(collection + a)) {
+
+                c = *(collection + a);
+                *(collection + a) = *(collection + b);
+                *(collection + b) = c;
+                contador++;
+            }
+        }
+    }
+    for (a = 0; a < count; a++)
+        printf("%d ", *(collection + a));
+
+    printf("\nNumero de intercambios necesarios: \n%d", contador);
+    return 0;
 }
+
 
 void insertion_sort(int *collection, int count)
 {
+    printf("\nINSERTION\n");
+    int a, b, c;
+    int contador = 0;
 
+    for (a = 1; a < count; a++){
+        c = collection[a];
+        for (b = a; b > 0 && collection[b-1] > c; b--){
+            collection[b] = collection[b-1];
+        }
+        collection[b] = c;
+        contador++;
+
+    }
+    for (a = 0; a < count; a++)
+        printf("%d ", *(collection + a));
+
+    printf("\nNumero de intercambios necesarios: \n%d", contador);
 }
+
 
 void reverse(int *collection, int count)
 {
+    printf("\nREVERSE\n");
+    int a, b, c;
+    for (a = 0, b = count-1; a < b; a++, b--){
+        c = *(collection+a);
+        *(collection+a)= *(collection+b);
+        *(collection+b) = c;
+    }
+    for (a=0; a < count; a++){
+        printf("%d ", *(collection +a));
+    }
 
 }
 
 int calulate_frequency(int *collection, int count, int target)
 {
+    printf("\nFREQUENCY\n");
+    int a, b;
+    int fr[count];
+    int visited = -1;
+
+    for (a = 0; a < count; a++) {
+        target = 1;
+        for(b = a + 1; b < count; b++){
+            if (*(collection+a)== (*(collection+b))){
+                target++;
+                fr[b]= visited;
+            }
+        }
+        if (fr[a]!=visited){
+            fr[a]= target;
+        }
+
+    }
+    printf(" Element | Frequency\n");
+    for (a = 0; a < count; a++){
+        if(fr[a]!=visited){
+            printf("     %d", *(collection+a));
+            printf("    |  ");
+            printf("  %d\n", fr[a]);
+        }
+    }
   return 0;
-}
+    /*
+ * MANERA MÁS SENCILLA FREQUENCY
+ * int a, b=0;
+ * for (a=0; a<count; a++){
+ *      if(+(collection + i) == target){
+ *          b++
+ *      }
+ *  }
+ *  printf("elemento: %d frequencia %d", target, b);
+ *  return 0;
+ */
+
+
